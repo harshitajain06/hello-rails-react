56ee948c8b02394d982f0e28ac8016f9913aed5e
Harshita Jain <harshitaforever06@gmail.com>
2023-05-05 18:16:48 +0530

    add suggested changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@
 
 Rails.application.routes.draw do
   get 'home/index'
-  resources :messages
-  # get '/messages/random_greeting', to: 'messages#random_greeting'
+  # resources :messages
+  get '/messages/random_greeting', to: 'messages#random_greeting'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      get 'messages', to: 'messages#index'
-    end
-  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     get 'messages', to: 'messages#index'
+  #   end
+  # end
   # Defines the root path route ("/")
   # root "articles#index"
 end
